b2b99d0159260ff30ae21437fda66fd413c178c9
K.Chen <43808882+bitifyChen@users.noreply.github.com>
2024-07-30 23:43:02 +0800

    Feat/index v1

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,35 +1,39 @@
 // Swiper JS
-import Swiper from 'swiper';
-import 'swiper/css';
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
 
 import '@/pages/index/index.scss';
 import '@/pages/news/index.scss';
 
 //Swiper
 const swiperTop = new Swiper('#top', {
-  loop: true, 
-  slidesPerView: 1,
-  spaceBetween: 10,
-    // pagination: {
-    //   el: '.swiper-pagination',
-    // },
-  });
-//Swiper
-const swiperNews = new Swiper('#news', {
-  loop: true, 
+  loop: true,
   slidesPerView: 1,
   spaceBetween: 10,
   autoplay: {
-    delay: 2500,
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '#top > .swiper-pagination',
+  },
+});
+//Swiper
+const swiperNews = new Swiper('#news', {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 5000,
     disableOnInteraction: false,
   },
   breakpoints: {
     768: {
       slidesPerView: 1.5,
-      spaceBetween: 20
-    }
-  }
-    // pagination: {
-    //   el: '.swiper-pagination',
-    // },
-  });
+      spaceBetween: 20,
+    },
+  },
+  pagination: {
+    el: 'section.news .swiper-pagination',
+  },
+});
