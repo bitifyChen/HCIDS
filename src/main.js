1f526685e331e58a274a8b08fbbe358235c18ffb
Kevin.Chen <kevin.,c>
2024-07-31 17:51:07 +0800

    AOS - init

--- src/main.js
+++ src/main.js
@@ -2,3 +2,14 @@ import './main.scss';
 //lucide
 import { createIcons, icons } from 'lucide';
 createIcons({ icons });
+
+//AOS
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
+
+
+window.addEventListener('load', () => {
+    AOS.init({
+        'offset': 200,
+    });
+});
