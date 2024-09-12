9091aa20a6210623ded0118619ddc9ac050b10b4
Kevin.Chen <kevin.,c>
2024-09-12 12:10:30 +0800

    links - qa

--- src/pages/links/index.js
+++ src/pages/links/index.js
@@ -1,1 +1,10 @@
+import { filter} from '@/common';
 import '@/pages/links/index.scss';
+
+document.addEventListener('DOMContentLoaded', () => {
+    //Filter
+    const _filterDom = document.querySelectorAll('.category ul li');
+ 
+    if (_filterDom) filter(_filterDom,'main')
+  });
+  
